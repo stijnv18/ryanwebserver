665255849e93a370d0de2566962245602bc9bb3c
Stijn verheyden <95874470+stijnv18@users.noreply.github.com>
2023-10-07 11:04:54 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Not eDating Ryan</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>I'm Not eDating Ryan</h1>
+        <p>Dear friends,</p>
+        <p>I want to make it clear that I am not in a romantic relationship with my friend Ryan. We are just good friends, and any rumors suggesting otherwise are not true.</p>
+        <p>Thank you for understanding.</p>
+    </div>
+</body>
+</html>
