6e54948085db28618633da3b826c859e0c935fe7
Stijn verheyden <95874470+stijnv18@users.noreply.github.com>
2023-10-07 11:41:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 </head>
 <body>
     <div class="container">
-        <h1>I'm Not eDating Ryan</h1>
-        <p>Dear friends,</p>
-        <p>I want to make it clear that I am not in a romantic relationship with my friend Ryan. We are just good friends, and any rumors suggesting otherwise are not true.</p>
-        <p>Thank you for understanding.</p>
+    <h1>Are you guys dumb im NOT FUCING EDATING RYAN FFS</h1>
+    <p>we only have send a couple thousend messages and have know each other for a couple of years now.</p>
+    <p>we are just friends and nothing more.</p>
+    <p>so stop asking me if im edating ryan.</p>
+    <p>this page is still WIP cause linux is fucking me over rn or im just stupid prob the last one</p>
     </div>
 </body>
 </html>
